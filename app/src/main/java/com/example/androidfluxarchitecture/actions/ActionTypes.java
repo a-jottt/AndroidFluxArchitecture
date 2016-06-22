33a5906b956d5d01ece9ce23f05e08db32bc0063
a-jottt <j.jasnowska@gmail.com>
2016-06-22 16:55:44 +0200

    Add ActionTypes and DataKeys to store actions and keys

---
+++ app/src/main/java/com/example/androidfluxarchitecture/actions/ActionTypes.java
@@ -0,0 +1,9 @@
+package com.example.androidfluxarchitecture.actions;
+
+/**
+ * Created by joanna on 22.06.16.
+ */
+public enum ActionTypes {
+
+    GET_REPOS_BY_LANGUAGE
+}
---
+++ app/src/main/java/com/example/androidfluxarchitecture/actions/DataKeys.java
@@ -0,0 +1,10 @@
+package com.example.androidfluxarchitecture.actions;
+
+/**
+ * Created by joanna on 22.06.16.
+ */
+public enum DataKeys implements DataKey {
+
+    REPO_LANGUAGE
+
+}
