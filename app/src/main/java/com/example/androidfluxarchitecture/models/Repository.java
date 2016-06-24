17eb9a8ff8182e75d02d7381c260f8bf92395895
a-jottt <j.jasnowska@gmail.com>
2016-06-24 14:33:18 +0200

    Add setters

--- app/src/main/java/com/example/androidfluxarchitecture/models/Repository.java
+++ app/src/main/java/com/example/androidfluxarchitecture/models/Repository.java
@@ -34,6 +34,11 @@ public class Repository {
         super();
     }
 
+    public Repository(String name, Integer stargazers_count) {
+        this.name = name;
+        this.stargazers_count = stargazers_count;
+    }
+
     public User getOwner() {
         return owner;
     }
@@ -68,5 +73,41 @@ public class Repository {
 
     public String getQuery() {
         return query;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public void setHtml_url(String html_url) {
+        this.html_url = html_url;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStargazers_count(Integer stargazers_count) {
+        this.stargazers_count = stargazers_count;
+    }
+
+    public void setWatchers_count(Integer watchers_count) {
+        this.watchers_count = watchers_count;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
     }
 }
